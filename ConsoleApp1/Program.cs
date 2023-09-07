bb85dd71a523833477e5fe7bfc0b48834d5dfd69
hanekawwa <tkachevn04@gmail.com>
2023-09-07 09:58:49 +0400

    Beta 1.4
    Most likely the final version.
    
    Fully functional with all options and settings implemented.
    
    Added:
    - Warnings for wrong user input
    - More Rotors and Reflectors to choose from
    - Displays now what Rotors and Reflectors are available to be chosen
    - Double step
    Changed:
    - Now options for Reflectors and Rotors depend on which machine are you using (Default army version or Kriegsmarine version)
    - No longer any critical errors crashing the program (as far as I have tested)
    - Now functions historically accurate
    - For 4th spot in Kriegsmarine version only special thin Rotors are available to be picked

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.IO;
 using System.Text.RegularExpressions;
+using ConsoleApp1;
+using static ConsoleApp1.library;
 
 class Rotor {
     public IDictionary<char, char> Wiring { get; set; } 
@@ -10,14 +12,16 @@ class Rotor {
     public List<int> Turnover { get; set; }
     public int Setting { get; set; }
     public IDictionary<char, char> Reverse { get; set; }
+    public bool Krieg { get; set; }
     
 
-    public Rotor(IDictionary<char, char> Wiring, int Count, List<int> Turnover, int Setting, IDictionary<char, char> Reverse) {
+    public Rotor(IDictionary<char, char> Wiring, int Count, List<int> Turnover, int Setting, IDictionary<char, char> Reverse, bool Krieg) {
         this.Wiring = Wiring;
         this.Count = Count;
         this.Turnover = Turnover;
         this.Setting = Setting;
         this.Reverse = Reverse;
+        this.Krieg = Krieg;
 
     }
     
@@ -55,79 +59,45 @@ class Program {
         { 'Z', 'Z' },
     };
     
-    public static Rotor Create(string Name) {
-        IDictionary<char, char> Temp = new Dictionary<char, char>() { };
-        IDictionary<char, char> TempR = new Dictionary<char, char>() { };
-        List<int> Turn = new List<int>() { };
-        String line;
-        try
-        {
-            StreamReader sr = new StreamReader("..\\"+Name+".txt");
-            for (int i = 0; i < 26; i++) {
-                line = sr.ReadLine();
-                Temp.Add(line[0], line[2]);
-                TempR.Add(line[2], line[0]);
-            }
-            line = sr.ReadLine();
-            line = sr.ReadLine();
-            string[]line1 = line.Split(',');
-            foreach(string i in line1)  {
-                int v = Int32.Parse(i);
-                Turn.Add(v);
-            }
-            sr.Close();
+    public static Rotor Create(string Name, bool Kriegsmarine) {
+        library lib = new library();
+        List<int> Turn = new List<int>();
+        IDictionary<char, char> Temp = new Dictionary<char, char>();
+        if (Kriegsmarine) {
+            Turn = lib.MasterDictKT[Name];
+            Temp = lib.MasterDictK[Name];
         }
-        catch(Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
-            return null;
+        else {
+            Turn = lib.MasterDictT[Name];
+            Temp = lib.MasterDict[Name];
         }
-        Console.WriteLine("Input Ring Setting (Ringstellung)");
-        int sett = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Input Ring Offset");
-        int offs = Int32.Parse(Console.ReadLine());
 
-        Rotor Temporary = new Rotor(Temp, offs, Turn, sett, TempR);
+        IDictionary<char, char> TempR = Temp.ToDictionary(x => x.Value, x => x.Key);
+        Console.WriteLine("Input Ring Setting (Ringstellung)(0-25)");
+        int sett = Int32.Parse(Console.ReadLine());
+        while (sett > 25) {
+            Console.WriteLine("Maximum number for Ring Setting is 25!");
+            Console.WriteLine("Input Ring Setting (Ringstellung)(0-25)");
+            sett = Int32.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Input Ring Offset(Grundstellung)(0-25)");
+        int offs = Int32.Parse(Console.ReadLine());
+        while (offs > 25) {
+            Console.WriteLine("Maximum number for Ring Offset is 25!");
+            Console.WriteLine("Input Ring Offset(Grundstellung)(0-25)");
+            offs = Int32.Parse(Console.ReadLine());
+        }
+        Rotor Temporary = new Rotor(Temp, offs, Turn, sett, TempR, Kriegsmarine);
         return Temporary;
     }
-
-    static IDictionary<char, char> UKW = new Dictionary<char, char>(){
-            { 'A', 'Q' },
-            { 'B', 'Y' },
-            { 'C', 'H' },
-            { 'D', 'O' },
-            { 'E', 'G' },
-            { 'F', 'N' },
-            { 'G', 'E' },
-            { 'H', 'C' },
-            { 'I', 'V' },
-            { 'J', 'P' },
-            { 'K', 'U' },
-            { 'L', 'Z' },
-            { 'M', 'T' },
-            { 'N', 'F' },
-            { 'O', 'D' },
-            { 'P', 'J' },
-            { 'Q', 'A' },
-            { 'R', 'X' },
-            { 'S', 'W' },
-            { 'T', 'M' },
-            { 'U', 'K' },
-            { 'V', 'I' },
-            { 'W', 'S' },
-            { 'X', 'R' },
-            { 'Y', 'B' },
-            { 'Z', 'L' },
-        };
-
-    private static List<IDictionary<char, char>> Reflector = new List<IDictionary<char, char>>() {UKW};
-
+    
     private static IDictionary<char, char> ActiveReflector;
     
     private static List<Rotor> ROTORS;
 
     static void Main() {
         while (true) {
+            library lib = new library();
             PLUG = new Dictionary<char, char>(){
                 { 'A', 'A' },
                 { 'B', 'B' },
@@ -165,10 +135,10 @@ class Program {
                 PlugboardCount = Int32.Parse(Console.ReadLine());
             }
             if (PlugboardCount != 0) {
-                Console.WriteLine("Input the plugboard settings(in format \"A-B\"):");
+                Console.WriteLine("Input the plugboard settings (in format \"A-B\"):");
                 for (int i = 0; i < PlugboardCount; i++) {
                     Console.WriteLine("Current connection: #" + (i + 1));
-                    string Temporary = Console.ReadLine();
+                    string Temporary = Console.ReadLine().ToUpper();
                     if (!r.IsMatch(Temporary)) {
                         i--;
                         Console.WriteLine("Please input in format \"A-B\" ");
@@ -190,30 +160,106 @@ class Program {
 
             ROTORS = new List<Rotor>() {
         };
-            Console.WriteLine("What reflector do you want to use?(Input number)");
-            Console.WriteLine("#1: UKW");
-            int ReflectorInput = Int32.Parse(Console.ReadLine());
-            while (ReflectorInput > Reflector.Count) {
-                Console.WriteLine("Reflector with that number does not exist!");
-                Console.WriteLine("What reflector do you want to use?");
-                Console.WriteLine("#1: UKW");
-                ReflectorInput = Int32.Parse(Console.ReadLine());
+            int amount = 0;
+            string Roman;
+            do {
+                Console.WriteLine("Do you want to use 3 (Default) or 4 (Kriegsmarine version) rotors?");
+                amount = Int32.Parse(Console.ReadLine());
+                if (amount == 3) {
+                    Console.WriteLine("What reflector do you want to use (Umkehrwalze)?(Input number)");
+                    int ccc = 0;
+                    foreach (string j in lib.MasterDictR.Keys) {
+                        ccc += 1;
+                        Console.WriteLine("#" + ccc + ": " + j);
+                    }
+                    
+                    int ReflectorInput = Int32.Parse(Console.ReadLine());
+                    while (ReflectorInput > lib.ReflectorK.Count || ReflectorInput==0) {
+                        Console.WriteLine("Reflector with that number does not exist!");
+                        Console.WriteLine("What reflector do you want to use (Umkehrwalze)?(Input number)");
+                        ccc = 0;
+                        foreach (string j in lib.MasterDictR.Keys) {
+                            ccc += 1;
+                            Console.WriteLine("#" + ccc + ": " + j);
+                        }
+
+                        ReflectorInput = Int32.Parse(Console.ReadLine());
+                    }
+
+                    ActiveReflector = lib.Reflector[ReflectorInput - 1];
+                }
+                else if (amount == 4) {
+                    Console.WriteLine("What thin reflector do you want to use (thin) (Umkehrwalze)?(Input number)");
+                    int ccc = 0;
+                    foreach (string j in lib.MasterDictKR.Keys) {
+                        ccc += 1;
+                        Console.WriteLine("#" + ccc + ": " + j);
+                    }
+                    
+                    int ReflectorInput = Int32.Parse(Console.ReadLine());
+                    while (ReflectorInput > lib.ReflectorK.Count||ReflectorInput==0) {
+                        Console.WriteLine("Reflector with that number does not exist!");
+                        Console.WriteLine("What reflector do you want to use (Umkehrwalze)?(Input number)");
+                        ccc = 0;
+                        foreach (string j in lib.MasterDictKR.Keys) {
+                            ccc += 1;
+                            Console.WriteLine("#" + ccc + ": " + j);
+                        }
+
+                        ReflectorInput = Int32.Parse(Console.ReadLine());
+                    }
+
+                    ActiveReflector = lib.ReflectorK[ReflectorInput - 1];
+                }
+                else {
+                    Console.WriteLine("Wrong input! Please choose either 3 or 4 rotors.");
+                    amount = 0;
+                }
+            } while (amount != 3 && amount != 4);
+
+
+            for (int i = 0; i < amount; i++) {
+            Rotor temp = null;
+            if (i == 3) {
+                Console.WriteLine("What should rotor #" + (i + 1) + " (thin) be? Choose out of the following:");
+                int co = 0;
+                foreach (string j in lib.MasterDictK.Keys) {
+                    co += 1;
+                    Console.Write("#"+co+": "+j+", ");
+                }
+                Roman = Console.ReadLine().ToUpper();
+                if (lib.MasterDictK.ContainsKey(Roman)) {
+                    temp = Create(Roman, true);
+                }
+                else {
+                    Console.WriteLine("No such rotor exists! Please input a valid rotor.");
+                    i--;
+                }
+                
+            }
+            else {
+                Console.WriteLine("What should rotor #" + (i + 1) + " be? Choose out of the following:");
+                int cc = 0;
+                foreach (string j in lib.MasterDict.Keys) {
+                    cc += 1;
+                    Console.Write("#" + cc + ": " + j + ", ");
+                }
+
+                Roman = Console.ReadLine().ToUpper();
+                if (lib.MasterDict.ContainsKey(Roman)) {
+                    temp = Create(Roman, false);
+                }
+                else {
+                    Console.WriteLine("No such rotor exists! Please input a valid rotor.");
+                    i--;
+                }
             }
 
-            ActiveReflector = Reflector[ReflectorInput-1];
-        Console.WriteLine("Do you want to use 3 (default) or 4 (kriegsmarine version) rotors?");
-        int amount = Int32.Parse(Console.ReadLine());
-        for (int i = 0; i < amount; i++) {
-            Console.WriteLine("What should rotor #" + (i + 1) + " be?");
-            string Roman = Console.ReadLine();
-            Rotor temp = Create(Roman);
+
             if (temp != null) {
                 ROTORS.Add(temp);
             }
-            else {
-                i--;
             }
-        }
         
             string input;
             string input_data = "";
@@ -223,10 +269,10 @@ class Program {
             while ((input = Console.ReadLine()) != null) {
                 input_data += input;
             }
-
+            Regex inp = new Regex("[A-Z]", RegexOptions.IgnoreCase);
             input_data = input_data.ToUpper();
             for (int i = 0; i < input_data.Length; i++) {
-                if (char.IsLetter(input_data[i])) {
+                if (inp.IsMatch(input_data[i]+"")) {
                     result += Encode(input_data[i]);
                     counter += 1;
                     if (counter % 5 == 0) {
@@ -251,7 +297,7 @@ class Program {
         int k = 0;
         ROTORS[k].Count += 1;
         while (k < ROTORS.Count) {
-            if (ROTORS[k].Turnover.Contains(ROTORS[k].Count)&& k!=ROTORS.Count-1) {
+            if (ROTORS[k].Turnover.Contains(ROTORS[k].Count)&& k!=ROTORS.Count-1 && !ROTORS[k+1].Krieg) {
                 ROTORS[k + 1].Count += 1;
                 if (ROTORS[k+1].Turnover.Contains(ROTORS[k+1].Count+1)&& k+1!=ROTORS.Count-1) {
                     ROTORS[k + 1].Count += 1;
